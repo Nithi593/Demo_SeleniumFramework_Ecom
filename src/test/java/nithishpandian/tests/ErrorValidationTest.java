cb7395faff6d24fe39d94d4efa5d8b130856f28b
Nithi593 <nithish.present@gmail.com>
2025-11-21 00:49:34 -0500

    Test 8

--- src/test/java/nithishpandian/tests/ErrorValidationTest.java
+++ src/test/java/nithishpandian/tests/ErrorValidationTest.java
@@ -60,7 +60,7 @@ public class ErrorValidationTest extends BaseTest {
 	
 	}
 	
-	// Test 7 for n8n automation
+	// Test 8 for n8n automation
 	
 	@Test
 	public void productErrorValidation2() throws IOException {

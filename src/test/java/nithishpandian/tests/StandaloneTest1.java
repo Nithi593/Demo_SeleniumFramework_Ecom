b2212ef2bccdb09af3b38d9bc64214d919f3ec6b
Nithi593 <nithish.present@gmail.com>
2025-11-27 21:47:18 -0500

    sample 13

--- src/test/java/nithishpandian/tests/StandaloneTest1.java
+++ src/test/java/nithishpandian/tests/StandaloneTest1.java
@@ -26,7 +26,7 @@ public class StandaloneTest1 {
 		driver.findElement(By.id("userEmail")).sendKeys("anshika@gmail.com");
 		driver.findElement(By.id("userPassword")).sendKeys("Iamking@000");
 		driver.findElement(By.id("login")).click();
-		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDri(driver,Duration.ofSeconds(5));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
 			List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
 		
@@ -38,7 +38,7 @@ public class StandaloneTest1 {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
 		//ng-animating
 		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
-		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+		driver.findElnt(By.cssSelector("[routerlink*='cart']")).click();
 		
 		List <WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));		
 	Boolean match = 	cartProducts.stream().anyMatch(cartProduct-> cartProduct.getText().equalsIgnoreCase(productName));
@@ -46,15 +46,15 @@ public class StandaloneTest1 {
 	driver.findElement(By.cssSelector(".totalRow button")).click();
 	
 	Actions a = new Actions(driver);
-	a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "india").build().perform();
+	a.sendKeys(driver.findElent(By.cssSelector("[placeholder='Select Country']")), "india").build().perform();
 	
 	wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
 	
 	driver.findElement(By.xpath("(//button[contains(@class,'ta-item')])[2]")).click();
-	driver.findElement(By.cssSelector(".action__submit")).click();
+	driver.findElement(By.cssSelector(".action__submit")).clicjjhk();
 	
 	String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
-	Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+	Assert.assertTrue(confirmmsage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
 	driver.close();
 	
 	}
